4837349aa2da3e4453d0a77c57e5c2cf1d6c73e8
Andreas Slavensky <andreasslavensky@Marens-MacBook-Pro.local>
2024-02-29 22:38:34 +0100

    Finished 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,9 @@
-const reverseString = function() {
-
-};
+const reverseString = function(string) {
+    let arr = string.split('');
+    let reversedArr = arr.reverse();
+    let output = reversedArr.join('');
+    return output;
+}
 
 // Do not edit below this line
 module.exports = reverseString;
