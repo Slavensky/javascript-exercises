fc8021ebc3c760ad26cafe64cc8572d2f9862f00
Andreas Slavensky <andreasslavensky@Marens-MacBook-Pro.local>
2024-03-01 19:27:48 +0100

    Finished 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
 
+    if (a < 0 || b < 0 || typeof a !== "number" || typeof b !== "number") {
+        return "ERROR";
+    }
+
+    let sum = 0;
+
+    let start;
+    let end;
+
+    if (a < b) {
+        start = a;
+        end = b;
+    } else {
+        start = b;
+        end = a;
+    }
+
+    for (let i=start; i < end + 1; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
