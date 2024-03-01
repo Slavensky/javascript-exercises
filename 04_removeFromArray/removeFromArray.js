b29d3c6c525adb9e10a0cb07dc2653ee4f44fcf9
Andreas Slavensky <andreasslavensky@Marens-MacBook-Pro.local>
2024-03-01 19:18:45 +0100

    Finished 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...args) {
+    let newArray = [];
+    for (const element of arr) {
+        if (!args.includes(element)) {
+            newArray[newArray.length] = element;
+        }
+    }
+    return newArray;
 };
 
 // Do not edit below this line
