7f20093bf8cb12fbf8428ec6b791bf8cc5589bd8
Andreas Slavensky <andreasslavensky@Marens-MacBook-Pro.local>
2024-03-01 19:31:27 +0100

    t

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,10 @@
-const leapYears = function() {
+const leapYears = function(number) {
+
+    if (number % 4 !== 0 || (number % 100 === 0 && number % 400 !== 0)) {
+        return false;
+    } else {
+        return true;
+    }
 
 };
 
