c3ef3b042f6a175d0e1c73eac51ce3b2a418cbeb
Andreas Slavensky <andreasslavensky@Marens-MacBook-Pro.local>
2024-04-01 02:09:00 +0200

    Finished exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let strLower = str.toLowerCase();
+    console.log(strLower);
+    let split = strLower.split(/[!,. ]+/).join('');
+    console.log(split);
+    let reversed = split.split('').reverse().join('');
+    let result = reversed === split;
+    return result;
 };
 
 // Do not edit below this line
